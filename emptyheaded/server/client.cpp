25dffdcc7531dba32fe5a6e91affd1a22d6ec843
Adam Perelman <adamperelman@gmail.com>
2015-04-02 15:55:05 -0700

    implement full server flow

--- emptyheaded/server/client.cpp
+++ emptyheaded/server/client.cpp
@@ -6,28 +6,39 @@
 #include "zmq.hpp"
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 int main ()
 {
   //  Prepare our context and socket
-  zmq::context_t context (1);
-  zmq::socket_t socket (context, ZMQ_REQ);
+  zmq::context_t context(1);
+  zmq::socket_t socket(context, ZMQ_REQ);
 
-  std::cout << "Connecting to hello world server…" << std::endl;
-  socket.connect ("tcp://localhost:5555");
+  std::cout << "connecting to server…" << std::endl << std::endl;
+  socket.connect("tcp://localhost:5555");
 
   //  Do 10 requests, waiting each time for a response
   for (int request_nbr = 0; request_nbr != 10; request_nbr++) {
-    const char* data = "run.cpp";
-    zmq::message_t request(strlen(data) + 1);
-    memcpy ((void *)request.data(), data, strlen(data));
-    std::cout << "Sending Hello " << request_nbr << "…" << std::endl;
-    socket.send (request);
+
+    // Read the data we'll want to send.
+    std::ifstream f("src/run.cpp");
+    std::stringstream buffer;
+    buffer << f.rdbuf();
+    const std::string data = buffer.str();
+
+    // Build the request.
+    zmq::message_t request(data.length() + 1);
+    memcpy((void*)request.data(), data.c_str(), data.length() + 1);
+
+    std::cout << "sending request..." << std::endl;
+    socket.send(request);
 
     //  Get the reply.
     zmq::message_t reply;
-    socket.recv (&reply);
-    std::cout << "Received World " << request_nbr << std::endl;
+    socket.recv(&reply);
+    std::cout << "received reply: " << (char*)reply.data() << std::endl << std::endl;
   }
+
   return 0;
 }
--- emptyheaded/server/server.cpp
+++ emptyheaded/server/server.cpp
@@ -1,11 +1,20 @@
 #include <cstdlib>
 #include <dlfcn.h>
 #include <iostream>
+#include <fstream>
 #include <unistd.h>
 #include <unordered_map>
 #include <string>
 
 #include "zmq.hpp"
+
+const size_t PATH_BUFFER_SIZE = 512;
+const char* CPP_FILE_NAME = "runnable.cpp";
+const char* OBJ_FILE_NAME = "runnable.o";
+const std::string COMPILE_COMMAND = (std::string("clang++ -O3 -dynamiclib ")
+                                     + CPP_FILE_NAME + " -o " + OBJ_FILE_NAME);
+
+typedef void (*run_t)(std::unordered_map<std::string, void*>& relations);
 
 int main () {
   //  Prepare our context and socket
@@ -13,50 +22,55 @@ int main () {
   zmq::socket_t socket (context, ZMQ_REP);
   socket.bind ("tcp://*:5555");
 
+  char dir_buffer[PATH_BUFFER_SIZE];
+  getcwd(dir_buffer, PATH_BUFFER_SIZE);
+  std::string dir(dir_buffer);
+
   std::unordered_map<std::string, void*> relations;
 
   assert(std::system(NULL));
 
-  std::system("clang++ -dynamiclib run.cpp -o run.o");
-
-  void* handle = dlopen("/Users/adamperelman/Code/chinstrap/emptyheaded/run.o", RTLD_NOW);
-  if (!handle) {
-    std::cerr << dlerror() << std::endl;
-    return 1;
-  }
-
-
-
-  typedef void (*run_t)(std::unordered_map<std::string, void*>& relations);
-
-  run_t run = (run_t)dlsym(handle, "run");
-
-  char* error = dlerror();
-  if (error)  {
-    std::cerr << error << std::endl;
-    return 1;
-  }
-
-  run(relations);
-
-  dlclose(handle);
-
   while (true) {
     zmq::message_t request;
 
-    //  Wait for next request from client
+    //  Wait for next request from client.
     socket.recv (&request);
     char* msg = (char*)request.data();
+    std::cout << "received a message" << std::endl;
 
-    std::cout << "Received " << msg << std::endl;
+    // Write the message to a file.
+    std::ofstream outfile(CPP_FILE_NAME);
+    outfile << msg;
+    outfile.close();
 
-    //  Do some 'work'
-	 sleep(1);
+    // Compile the file.
+    std::system(COMPILE_COMMAND.c_str());
+
+    // Open and run the file.
+    void* handle = dlopen((dir + "/" + OBJ_FILE_NAME).c_str(), RTLD_NOW);
+    if (!handle) {
+      std::cerr << dlerror() << std::endl;
+      return 1;
+    }
+
+    run_t run = (run_t)dlsym(handle, "run");
+
+    char* error = dlerror();
+    if (error)  {
+      std::cerr << error << std::endl;
+      dlclose(handle);
+      return 1;
+    }
+
+    run(relations);
+
+    dlclose(handle);
 
     //  Send reply back to client
-    zmq::message_t reply (5);
-    memcpy ((void *) reply.data (), "World", 5);
-    socket.send (reply);
+    const char* reply_message = "executed file successfully";
+    zmq::message_t reply (strlen(reply_message) + 1);
+    memcpy ((void*)reply.data(), reply_message, strlen(reply_message) + 1);
+    socket.send(reply);
   }
   return 0;
 }
