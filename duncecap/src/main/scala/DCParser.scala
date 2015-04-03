329ee5e0037a5d0b8799db860b789bc4b84f187c
Susan Tu <susanctu@gmail.com>
2015-04-03 14:54:58 -0700

    fixing parsing of strings with escaped characters

--- duncecap/src/main/scala/DCParser.scala
+++ duncecap/src/main/scala/DCParser.scala
@@ -12,10 +12,21 @@ class DCParser extends RegexParsers {
   def notLastAttr = identifier ~ (":" ~> typename) ~ ("," ~> attrDefList) ^^ {case a~t~rest => rest + (a -> t)}
   def lastAttr = identifier ~ (":" ~> typename) ^^ {case a~t => Map(a -> t)}
 
-  def notQuote : Parser[String] = """[^\"]""".r
-  def escapedQuote : Parser[String] = """\\\"""".r
+
+  /**
+   * strings may contain quotes, which must be escaped
+   * any other character escaped is just itself (i.e., \c is the same as c)
+   * except for \t (tab) and \n (newline)
+   *
+   * unescaped newlines will not be allowed by the REPL
+   * (but this restriction isn't implemented here, since the repl will assume unescaped newline means end of statement)
+   */
+  def tab : Parser[String] = """\\t""".r ^^ {case _ => "\t"}
+  def newline : Parser[String] = """\\n""".r ^^ {case _ => "\n"}
+  def escapedChar : Parser[String] = "\\" ~> """.""".r ^^ {case c => c}
+  def notEscaped : Parser[String] = """[^\"]""".r
   def validStringContents : Parser[String] = notEndOfString | endOfString
-  def notEndOfString  : Parser[String] = ((escapedQuote|notQuote) ~ validStringContents) ^^ {case c~rest => c+rest}
+  def notEndOfString  : Parser[String] = ((tab|newline|escapedChar|notEscaped) ~ validStringContents) ^^ {case c~rest => c+rest}
   def endOfString  : Parser[String] = """""".r
   def string = "\"" ~> validStringContents <~ "\""
 
--- duncecap/src/test/DCParserTest.scala
+++ duncecap/src/test/DCParserTest.scala
@@ -4,6 +4,16 @@ import org.scalatest.FunSuite
 class DCParserTest extends FunSuite {
   final val parser : DCParser = new DCParser
 
+  test("Strings get parsed correctly") {
+    val tabThenNewlineThenChars = parser.parseAll(parser.string, "\"\\t\\na\\sd\\fghjkl\"")
+    assert(tabThenNewlineThenChars.successful)
+    assertResult("\t\nasdfghjkl")(tabThenNewlineThenChars.get)
+
+    val escapedBackslashBeforeEndQuote = parser.parseAll(parser.string, "\"foo\\\\\"")
+    assert(escapedBackslashBeforeEndQuote.successful)
+    assertResult("foo\\")(escapedBackslashBeforeEndQuote.get)
+  }
+
   test("Can correctly parse a simple load statement") {
     val result1 = parser.parseAll(parser.loadExpr, "R(a:long) <- load(\"filename\", tsv)")
     assert(result1.successful)
@@ -11,17 +21,17 @@ class DCParserTest extends FunSuite {
 
     val result2 = parser.parseAll(parser.loadExpr, "R2(a:float,b:long, c:string) <- load (\"fil\\\"ename\",csv  )")
     assert(result2.successful)
-    assertResult(new ASTLoadExpression(new ASTRelation(Map(("c", "string"), ("b", "long"), ("a", "float"))),"fil\\\"ename","csv"))(result2.get)
+    assertResult(new ASTLoadExpression(new ASTRelation(Map(("c", "string"), ("b", "long"), ("a", "float"))),"fil\"ename","csv"))(result2.get)
   }
 
   test("Rejects malformed load statements") {
-    val result1 = parser.parseAll(parser.loadExpr, "R(a:long,b:long) <- load(\"filename\", unrecognizedFormat)")
-    assert(!result1.successful)
+    val unrecognizedFormat = parser.parseAll(parser.loadExpr, "R(a:long,b:long) <- load(\"filename\", unrecognizedFormat)")
+    assert(!unrecognizedFormat.successful)
 
-    val result2 = parser.parseAll(parser.loadExpr, "R(a:long,b:unrecognizedType) <- load(\"filename\", tsv)")
-    assert(!result2.successful)
+    val unrecognizedType = parser.parseAll(parser.loadExpr, "R(a:long,b:unrecognizedType) <- load(\"filename\", tsv)")
+    assert(!unrecognizedType.successful)
 
-    val result3 = parser.parseAll(parser.loadExpr, "R() <- load(\"filename\", tsv)") // can't load without schema
-    assert(!result3.successful)
+    val noSchema = parser.parseAll(parser.loadExpr, "R() <- load(\"filename\", tsv)")
+    assert(!noSchema.successful)
   }
 }
