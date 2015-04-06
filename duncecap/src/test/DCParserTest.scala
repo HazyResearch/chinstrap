b4a6aa5d2aeecc944b044883e5945a55a04dfbab
Susan Tu <susanctu@gmail.com>
2015-04-05 19:11:03 -0700

    parse the rest of the query language, as written up in the drive doc

--- duncecap/src/test/DCParserTest.scala
+++ duncecap/src/test/DCParserTest.scala
@@ -1,4 +1,4 @@
-import DunceCap.{ASTRelation, ASTLoadExpression, DCParser}
+import DunceCap._
 import org.scalatest.FunSuite
 
 class DCParserTest extends FunSuite {
@@ -6,31 +6,66 @@ class DCParserTest extends FunSuite {
   test("Strings get parsed correctly") {
     val tabThenNewlineThenChars = DCParser.parseAll(DCParser.string, "\"\\t\\na\\sd\\fghjkl\"")
     assert(tabThenNewlineThenChars.successful)
-    assertResult("\t\nasdfghjkl")(tabThenNewlineThenChars.get)
+    assertResult(new ASTStringLiteral("\t\nasdfghjkl"))(tabThenNewlineThenChars.get)
 
     val escapedBackslashBeforeEndQuote = DCParser.parseAll(DCParser.string, "\"foo\\\\\"")
     assert(escapedBackslashBeforeEndQuote.successful)
-    assertResult("foo\\")(escapedBackslashBeforeEndQuote.get)
+    assertResult(new ASTStringLiteral("foo\\"))(escapedBackslashBeforeEndQuote.get)
   }
 
   test("Can correctly parse a simple load statement") {
-    val result1 = DCParser.parseAll(DCParser.loadExpr, "R(a:long) <- load(\"filename\", tsv)")
+    val result1 = DCParser.parseAll(DCParser.loadStatement, "R(a:long) <- load(\"filename\", tsv)")
     assert(result1.successful)
-    assertResult(new ASTLoadExpression(new ASTRelation(Map(("a", "long"))),"filename","tsv"))(result1.get)
+    assertResult(new ASTLoadStatement(new ASTRelation("R", Map(("a", Some("long")))),new ASTStringLiteral("filename"),"tsv"))(result1.get)
 
-    val result2 = DCParser.parseAll(DCParser.loadExpr, "R2(a:float,b:long, c:string) <- load (\"fil\\\"ename\",csv  )")
+    val result2 = DCParser.parseAll(DCParser.loadStatement, "R2(a:float,b:long, c:string) <- load (\"fil\\\"ename\",csv  )")
     assert(result2.successful)
-    assertResult(new ASTLoadExpression(new ASTRelation(Map(("c", "string"), ("b", "long"), ("a", "float"))),"fil\"ename","csv"))(result2.get)
+    assertResult(new ASTLoadStatement(new ASTRelation("R2", Map(("c", Some("string")), ("b", Some("long")), ("a", Some("float")))), new ASTStringLiteral("fil\"ename"),"csv"))(result2.get)
   }
 
   test("Rejects malformed load statements") {
-    val unrecognizedFormat = DCParser.parseAll(DCParser.loadExpr, "R(a:long,b:long) <- load(\"filename\", unrecognizedFormat)")
+    val unrecognizedFormat = DCParser.parseAll(DCParser.loadStatement, "R(a:long,b:long) <- load(\"filename\", unrecognizedFormat)")
     assert(!unrecognizedFormat.successful)
 
-    val unrecognizedType = DCParser.parseAll(DCParser.loadExpr, "R(a:long,b:unrecognizedType) <- load(\"filename\", tsv)")
+    val unrecognizedType = DCParser.parseAll(DCParser.loadStatement, "R(a:long,b:unrecognizedType) <- load(\"filename\", tsv)")
     assert(!unrecognizedType.successful)
 
-    val noSchema = DCParser.parseAll(DCParser.loadExpr, "R() <- load(\"filename\", tsv)")
+    val noSchema = DCParser.parseAll(DCParser.loadStatement, "R() <- load(\"filename\", tsv)")
     assert(!noSchema.successful)
+  }
+
+  test("Can correctly parse joins and selects") {
+    val justJoin  = DCParser.parseAll(DCParser.statement, "T(a,b,c) <- R(a,b), R(b,c), R(a,c)")
+    assert(justJoin.successful)
+    val triangleJoinList = List[ASTRelation](
+      new ASTRelation("R", Map(("b", None),("a", None))),
+      new ASTRelation("R", Map(("c", None),("b", None))),
+      new ASTRelation("R", Map(("c", None),("a", None))))
+    assertResult(new ASTAssignStatement(
+      new ASTRelation("T", Map(("c", None),("b", None),("a", None))),
+      new ASTJoinAndSelect(triangleJoinList,List[ASTCriterion]())))(justJoin.get)
+
+    val justSelect = DCParser.parseAll(DCParser.statement, "U(a,b) <- R(a,b), a = \"Chris\"")
+    assert(justSelect.successful)
+    assertResult(new ASTAssignStatement(
+      new ASTRelation("U", Map(("a", None), ("b", None))),
+      new ASTJoinAndSelect(
+        List[ASTRelation](new ASTRelation("R", Map(("a", None), ("b", None)))),
+        List[ASTCriterion](new ASTEq(new ASTScalar("a"), new ASTStringLiteral("Chris"))))))(justSelect.get)
+
+    val prunedTriangleQuery = DCParser.parseAll(DCParser.statement, "T(a,b,c) <- R(a,b), R(b,c), R(a,c), a < b, b < c, a < c")
+    assert(prunedTriangleQuery.successful)
+    val trianglePruneList = List[ASTCriterion](
+      new ASTLess(new ASTScalar("a"), new ASTScalar("b")),
+      new ASTLess(new ASTScalar("b"), new ASTScalar("c")),
+      new ASTLess(new ASTScalar("a"), new ASTScalar("c")))
+    val expectedASTForPrunedTriangle = new ASTAssignStatement(
+      new ASTRelation("T", Map(("c", None),("b", None),("a", None))),
+      new ASTJoinAndSelect(triangleJoinList,trianglePruneList))
+    assertResult(expectedASTForPrunedTriangle)(prunedTriangleQuery.get)
+
+    val countPrunedTriangles = DCParser.parseAll(DCParser.statement, "result <- COUNT(R(a,b), R(b,c), R(a,c), a < b, b < c, a < c)")
+    val expectedASTForCount = new ASTAssignStatement(new ASTScalar("result"), new ASTCount(new ASTJoinAndSelect(triangleJoinList,trianglePruneList)))
+    assertResult(expectedASTForCount)(countPrunedTriangles.get)
   }
 }
