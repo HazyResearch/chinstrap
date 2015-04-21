46ccd5ded834cf8194a57c32905761eccd66a664
Susan Tu <susanctu@gmail.com>
2015-04-21 12:41:07 -0700

    make binary for standalone generated cpp file

--- duncecap/src/main/scala/QueryCompiler.scala
+++ duncecap/src/main/scala/QueryCompiler.scala
@@ -1,6 +1,7 @@
 package DunceCap
 
 import java.io.{FileWriter, File, BufferedWriter}
+import sys.process._
 
 /**
  * You can compile the output of this class by doing
@@ -23,10 +24,20 @@ object QueryCompiler extends App {
         val codeStringBuilder = new CodeStringBuilder
         CodeGen.emitHeaderAndCodeForAST(codeStringBuilder, ast)
         CodeGen.emitMainMethod(codeStringBuilder)
-        val file = new File(s"""../emptyheaded/${outputFilename}""")
+        val cppFilepath = s"""../emptyheaded/apps/generated/${outputFilename}.cpp"""
+        val binaryFilepath = s"""../emptyheaded/bin/${outputFilename}.exe"""
+        val file = new File(cppFilepath)
         val bw = new BufferedWriter(new FileWriter(file))
         bw.write(codeStringBuilder.toString)
         bw.close()
+
+        val result = s"""clang++ ${cppFilepath} -o ${binaryFilepath} -std=c++11 -march=native -mtune=native -I../emptyheaded/src""" !
+
+        if (result != 0) {
+          println("FAILURE: Compilation errors.")
+        } else {
+          println("SUCCESS.")
+        }
       }
       case x => println(x)
     }
