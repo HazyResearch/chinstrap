90d4eaa61172c62b4994500b99d28955f574582b
Susan Tu <susanctu@gmail.com>
2015-04-26 15:29:28 -0700

    moving over last quarter's GHD code, need to resolve some naming conflicts

---
+++ duncecap/src/main/scala/GHDSolver.scala
@@ -0,0 +1,105 @@
+package DunceCap
+
+import scala.collection.mutable
+
+object GHDSolver {
+  def getAttrSet(rels: List[Relation]): Set[String] = {
+    return rels.foldLeft(Set[String]())(
+      (accum: Set[String], rel : Relation) => accum | rel.attrs.toSet[String])
+  }
+
+  private def getConnectedComponents(rels: mutable.Set[Relation], comps: List[List[Relation]], ignoreAttrs: Set[String]): List[List[Relation]] = {
+    if (rels.isEmpty) return comps
+    val component = getOneConnectedComponent(rels, ignoreAttrs)
+    return getConnectedComponents(rels, component::comps, ignoreAttrs)
+  }
+
+  private def getOneConnectedComponent(rels: mutable.Set[Relation], ignoreAttrs: Set[String]): List[Relation] = {
+    val curr = rels.head
+    rels -= curr
+    return DFS(mutable.LinkedHashSet[Relation](curr), curr, rels, ignoreAttrs)
+  }
+
+  private def DFS(seen: mutable.Set[Relation], curr: Relation, rels: mutable.Set[Relation], ignoreAttrs: Set[String]): List[Relation] = {
+    for (rel <- rels) {
+      // if these two hyperedges are connected
+      if (!((curr.attrs.toSet[String] & rel.attrs.toSet[String]) &~ ignoreAttrs).isEmpty) {
+        seen += curr
+        rels -= curr
+        DFS(seen, rel, rels, ignoreAttrs)
+      }
+    }
+    return seen.toList
+  }
+
+  // Visible for testing
+  def getPartitions(leftoverBags: List[Relation], // this cannot contain chosen
+                    chosen: List[Relation],
+                    parentAttrs: Set[String],
+                    tryBagAttrSet: Set[String]): Option[List[List[Relation]]] = {
+    // first we need to check that we will still be able to satisfy
+    // the concordance condition in the rest of the subtree
+    for (bag <- leftoverBags.toList) {
+      if (!(bag.attrs.toSet[String] & parentAttrs).subsetOf(tryBagAttrSet)) {
+        return None
+      }
+    }
+
+    // if the concordance condition is satisfied, figure out what components you just
+    // partitioned your graph into, and do ghd on each of those disconnected components
+    val relations = mutable.LinkedHashSet[Relation]() ++ leftoverBags
+    return Some(getConnectedComponents(relations, List[List[Relation]](), getAttrSet(chosen).toSet[String]))
+  }
+
+  /**
+   * @param partitions
+   * @param parentAttrs
+   * @return Each list in the returned list could be the children of the parent that we got parentAttrs from
+   */
+  private def getListsOfPossibleSubtrees(partitions: List[List[Relation]], parentAttrs: Set[String]): List[List[GHDNode]] = {
+    assert(!partitions.isEmpty)
+    val subtreesPerPartition: List[List[GHDNode]] = partitions.map((l: List[Relation]) => getMinFractionalWidthDecomposition(l, parentAttrs))
+    val foldFunc: (List[List[GHDNode]], List[GHDNode]) => List[List[GHDNode]]
+    = (accum: List[List[GHDNode]], subtreesForOnePartition: List[GHDNode]) => subtreesForOnePartition.map(
+      (subtreeForOnePartition: GHDNode) => accum.map((children: List[GHDNode]) => subtreeForOnePartition::children).flatten)
+    return subtreesPerPartition.foldLeft(List[List[GHDNode]](List[GHDNode]()))(foldFunc)
+  }
+
+  private def getMinFractionalWidthDecomposition(rels: List[Relation], parentAttrs: Set[String]): List[GHDNode] =  {
+    val treesFound = mutable.ListBuffer[GHDNode]()
+    for (tryNumRelationsTogether <- (1 to rels.size).toList) {
+      for (bag <- rels.combinations(tryNumRelationsTogether).toList) {
+        val leftoverBags = rels.toSet[Relation] &~ bag.toSet[Relation]
+        if (leftoverBags.toList.isEmpty) {
+          val newNode = new GHDNode(bag)
+          treesFound.append(newNode)
+        } else {
+          val bagAttrSet = getAttrSet(bag)
+          val partitions = getPartitions(leftoverBags.toList, bag, parentAttrs, bagAttrSet)
+          if (partitions.isDefined) {
+            // lists of possible children for |bag|
+            val possibleSubtrees: List[List[GHDNode]] = getListsOfPossibleSubtrees(partitions.get, bagAttrSet)
+            for (subtrees <- possibleSubtrees) {
+              val newNode = new GHDNode(bag)
+              newNode.children = subtrees
+              treesFound.append(newNode)
+            }
+          }
+        }
+      }
+    }
+    return treesFound.toList
+  }
+
+  def getMinFractionalWidthDecomposition(rels: List[Relation]): List[GHDNode] = {
+    return getMinFractionalWidthDecomposition(rels, Set[String]())
+  }
+}
+
+
+
+
+
+
+
+
---
+++ duncecap/src/main/scala/GHDWriter.scala
@@ -0,0 +1,120 @@
+package DunceCap
+
+import java.io.{FileWriter, BufferedWriter, File}
+
+import DunceCap.{GHDNode, GHDSolver, Relation}
+
+object GHDWriter {
+  def main(args: Array[String]): Unit = {
+    //triangle()
+    //tadpole3_1()
+    // tadpole4_1()
+    //fourClique()
+
+    val freebase: List[Relation] = List(
+      new Relation(List("a", "b")),
+      new Relation(List("b", "c")),
+      new Relation(List("c", "d")),
+      new Relation(List("d", "e")),
+      new Relation(List("a", "e")),
+      new Relation(List("e", "f")),
+      new Relation(List("f", "g")),
+      new Relation(List("g", "h")),
+      new Relation(List("h", "a")))
+    printAllMinWidthDecomps(freebase, "freebase")
+    val q7: List[Relation] = List(
+      new Relation(List("a", "b")),
+      new Relation(List("b", "c", "d")),
+      new Relation(List("b", "e")),
+      new Relation(List("a", "c")))
+    printAllMinWidthDecomps(q7, "q7")
+    val barbell: List[Relation] = List(
+      new Relation(List("a", "b")),
+      new Relation(List("b", "c")),
+      new Relation(List("c", "a")),
+      new Relation(List("a2", "b2")),
+      new Relation(List("b2", "c2")),
+      new Relation(List("c2", "a2")),
+      new Relation(List("a", "a2")))
+    printAllMinWidthDecomps(barbell, "barbell")
+  }
+
+  def printAllMinWidthDecomps(rels : List[Relation], name : String): Unit = {
+    case class Precision(val p:Double)
+    class withAlmostEquals(d:Double) {
+      def ~=(d2:Double)(implicit p:Precision) = (d-d2).abs <= p.p
+    }
+    implicit def add_~=(d:Double) = new withAlmostEquals(d)
+    implicit val precision = Precision(0.001)
+
+    val solver = GHDSolver
+    val decompositions = solver.getMinFractionalWidthDecomposition(rels)
+    val fhws = decompositions.map((root : GHDNode) => (root.fractionalScoreTree(), root))
+    val minScore = fhws.unzip._1.min
+    println(s"""minScore: ${minScore}""")
+    val minFhws = fhws.filter((scoreAndNode : (Double, GHDNode)) => scoreAndNode._1 ~= minScore)
+    minFhws.map({case (fhw, root)=> root.reorderAttributes()})
+    minFhws.unzip._2.zipWithIndex.map({case (root, index) => print(root, "../query_plans/gen/" + name +"_generated" + index + ".json")})
+  }
+
+  def tadpole3_1(): Unit = {
+    val solver = GHDSolver
+    val TADPOLE: List[Relation] = List(
+      new Relation(List("a", "b")),
+      new Relation(List("b", "c")),
+      new Relation(List("c", "a")),
+      new Relation(List("a", "e")))
+    val decompositions = solver.getMinFractionalWidthDecomposition(TADPOLE)
+    val fhws = decompositions.map((root : GHDNode) => root.fractionalScoreTree())
+    decompositions.map((root : GHDNode) => root.reorderAttributes())
+    decompositions.zip(fhws).map({case (root, fhw) => print(root, "../query_plans/gen/tadpole3_1_generated" + fhw + ".json")})
+  }
+
+  def tadpole4_1(): Unit = {
+    val solver = GHDSolver
+    val TADPOLE: List[Relation] = List(
+      new Relation(List("a", "b")),
+      new Relation(List("b", "c")),
+      new Relation(List("c", "a")),
+      new Relation(List("a", "e")))
+    val decompositions = solver.getMinFractionalWidthDecomposition(TADPOLE)
+    val fhws = decompositions.map((root : GHDNode) => root.fractionalScoreTree())
+    decompositions.map((root : GHDNode) => root.reorderAttributes())
+    decompositions.zip(fhws).zipWithIndex.map({case (plan_info, index) => print(plan_info._1, "../query_plans/gen/lollipop3_1_generated" + plan_info._2 + "_" + index + ".json")})
+  }
+
+  def fourClique(): Unit = {
+    val solver = GHDSolver
+    val CLIQUE: List[Relation] = List(
+      new Relation(List("a", "b")),
+      new Relation(List("b", "c")),
+      new Relation(List("c", "a")),
+      new Relation(List("b", "d")),
+      new Relation(List("a", "d")),
+      new Relation(List("d", "c")))
+    val decompositions = solver.getMinFractionalWidthDecomposition(CLIQUE)
+    val fhws = decompositions.map((root : GHDNode) => root.fractionalScoreTree())
+    decompositions.map((root : GHDNode) => root.reorderAttributes())
+    decompositions.zip(fhws).map({case (root, fhw) => print(root, "../query_plans/gen/clique4_generated" + fhw + ".json")})
+  }
+
+  def triangle(): Unit = {
+    val solver = GHDSolver
+    val TRI: List[Relation] = List(
+      new Relation(List("a", "b")),
+      new Relation(List("b", "c")),
+      new Relation(List("c", "a")))
+    val decompositions = solver.getMinFractionalWidthDecomposition(TRI)
+    val fhws = decompositions.map((root : GHDNode) => root.fractionalScoreTree())
+    decompositions.map((root : GHDNode) => root.reorderAttributes())
+    decompositions.zip(fhws).map({case (root, fhw) => print(root, "../query_plans/gen/triangle_generated" + fhw + ".json")})
+  }
+
+  def print(root: GHDNode, filename: String) = {
+    val json = root.toJson()
+    val file = new File(filename)
+    val bw = new BufferedWriter(new FileWriter(file))
+    bw.append(json.spaces2)
+    bw.close()
+  }
+}
