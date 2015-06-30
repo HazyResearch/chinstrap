04ec760924e48e814530eaa4cf7be6425dce7e2e
craberger <craberger@gmail.com>
2015-06-30 14:21:46 -0700

    Should fail just testing.

--- tests/test.sh
+++ tests/test.sh
@@ -1,7 +1,12 @@
+set -e
 sbt update
 if [ ! -d "tests/data" ]; then
   # Control will enter here if $DIRECTORY doesn't exist.
   cd tests && tar -zxvf data.tar.gz && cd ..
 fi
 ./compile.sh queries/lubm/lubm1.datalog
-cd ./emptyheaded && make clean && make tests TRAVIS=true && cd ..
+cd ./emptyheaded && make clean && make tests TRAVIS=true 
+for app in "test_lubm1" "test_undirected_triangle_counting" "test_undirected_triangle_listing"; do
+  ./bin/${app}
+done
+cd ..
