640b969b72d6e0afc52ad09d44c75fcf5e44d875
craberger <craberger@gmail.com>
2015-06-30 14:33:26 -0700

    Updated file path in LUBM 1.

--- tests/test.sh
+++ tests/test.sh
@@ -4,9 +4,13 @@ if [ ! -d "tests/data" ]; then
   # Control will enter here if $DIRECTORY doesn't exist.
   cd tests && tar -zxvf data.tar.gz && cd ..
 fi
+
 ./compile.sh queries/lubm/lubm1.datalog
+
 cd ./emptyheaded && make clean && make tests TRAVIS=true 
-for app in "test_lubm1" "test_undirected_triangle_counting" "test_undirected_triangle_listing"; do
+NAMES=$(find tests -name '*.cpp' -exec basename {} \;)
+for name in $NAMES; do
+  app=`echo "${name}" | cut -d'.' -f1`
   ./bin/${app}
 done
 cd ..
