d7dd4d883059e7e512692cf9d4a662bd42451957
craberger <craberger@gmail.com>
2015-10-04 22:53:29 -0700

    New numbers for SSSP.

--- emptyheaded/src/TransitiveClosure.hpp
+++ emptyheaded/src/TransitiveClosure.hpp
@@ -8,6 +8,8 @@
 
 #include "Trie.hpp"
 
+#define NEW_TC
+
 namespace tc {
 
   template<class T, class R,typename J>
@@ -22,7 +24,7 @@ namespace tc {
     assert(graph->num_levels == 2);
     
     ///just stuff to setup the visited set
-    const size_t visited_num_bytes = range_bitset::get_number_of_bytes(num_distinct,num_distinct);
+    const size_t visited_num_bytes = PADDING*range_bitset::get_number_of_bytes(num_distinct,num_distinct);
     uint8_t* bs_data = output_buffer->get_next(0,visited_num_bytes);
     memset(bs_data,(uint8_t)0,visited_num_bytes);
     Set<range_bitset> vs = new(output_buffer->get_next(0, sizeof(Set<range_bitset>))) Set<range_bitset>(bs_data,num_distinct+1,num_distinct,visited_num_bytes,type::RANGE_BITSET);
@@ -42,28 +44,41 @@ namespace tc {
     //copy the data in the head set to the frontier and mark it as visited
     uint32_t* frontier = new uint32_t[num_distinct];
     uint32_t frontier_size = 1;
-    ops::atomic_union(visited->set,start);
+    ops::atomic_union(&visited->set,PADDING*start);
     visited->set_data(0,start,0);
     frontier[0] = start;
 
+    uint64_t* A = (uint64_t*)(visited->set.data+sizeof(uint64_t));
+
+    auto it_timer = debug::start_clock();
     size_t iteration = 0;
     while(frontier_size != 0){
       std::cout  << "ITERATION: " << iteration << " FRONTIER SIZE: " << frontier_size << std::endl;
+      //NUM_THREADS = 1;
+      //NUM_THREADS = 48; 
       par::for_range(0,frontier_size,100,[&](size_t tid, size_t i){
+        (void)tid;
         const uint32_t f = frontier[i];
         TrieBlock<T,R>* level2 = graph->head->get_block(f);
         level2->set.foreach([&](uint32_t l2){
-          //union in element and return index if element does not exist in the set
-          // and return -1 if it exists in the set (no work to do in the union)
-          if(ops::atomic_union(visited->set,l2)){
-            //std::cout << "data: " << visited->get_data(f) << " " << init << std::endl;
-            visited->set_data(0,l2,join(visited->get_data(f),init)); //index does not matter this is dense
-            const size_t buffer_index = frontier_sizes[tid*PADDING];
-            frontier_buffer[tid][buffer_index] = l2;
-            frontier_sizes[tid*PADDING]++;
+          const uint32_t data_elem = l2*PADDING;
+          const size_t word = range_bitset::word_index(data_elem);
+          const uint64_t set_bit = ((uint64_t) 1 << (data_elem%BITS_PER_WORD));
+          const uint64_t old_value = A[word];
+          if(!(old_value & set_bit)) {
+            const uint64_t old_value2 = __sync_fetch_and_or((uint64_t*)&A[word],set_bit);
+            //A64[word-offset] |= set_bit;
+            if(!(old_value2 & set_bit)){
+              //A[word] |= set_bit;
+              visited->set_data(0,l2,join(visited->get_data(f),init)); //index does not matter this is dense
+              const size_t buffer_index = frontier_sizes[tid*PADDING];
+              frontier_buffer[tid][buffer_index] = l2;
+              frontier_sizes[tid*PADDING]++;
+            }
           }
         });
       });
+
       //reconstruct frontier buffer
       frontier_size = 0;
       for(size_t t = 0 ; t < NUM_THREADS; t++){
@@ -75,6 +90,7 @@ namespace tc {
       iteration++;
     }
 
+    debug::stop_clock("Iteration " + iteration,it_timer);
     range_bitset::set_indices(visited->set.data,visited->set.cardinality,visited->set.number_of_bytes,visited->set.type);
     /*
     visited->set.foreach_index([&](uint32_t i, uint32_t d){
@@ -82,6 +98,7 @@ namespace tc {
     });
     */
     //reconstruct the visited set indicies, package into a trie and return
+
     return (TrieBlock<T,R>*)visited;
   }
 
